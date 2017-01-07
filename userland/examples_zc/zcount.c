f7387f99dcd0e48c35deee2e7a032eac33925681
taki <iddyiddyiddy+kitkat@gmail.com>
2017-01-07 15:50:58 +1300

    fixed include

--- userland/examples_zc/zcount.c
+++ userland/examples_zc/zcount.c
@@ -72,7 +72,7 @@ u_int64_t threshold_min_count = 0, threshold_max_count = 0;
 volatile u_int64_t *pulse_timestamp_ns;
 
 /* lock buffer */
-#include <lock_buffer.c>
+#include <"lock_buffer.c">
 u_char use_lock_buffer = 0; 
 struct lock_buffer * lb_buffer;
 
