8cd5e4f06ec1ad69b61a0c55c1e1c7fe231e0143
taki <iddyiddyiddy+kitkat@gmail.com>
2017-01-31 06:15:58 +1300

    extracts ofp type and xid now

--- userland/examples_zc/zcount.c
+++ userland/examples_zc/zcount.c
@@ -267,6 +267,7 @@ void *packet_consumer_thread(void *user) {
           memcpy(&lb_it->hi.src, &pkt_data[22], 6);
           
           ip_hdr = (struct iphdr *) &pkt_data[32];
+          printf("_%X_\n", ntohs(ip_hdr->protocol));
           if (ip_hdr->protocol == 60) {// tcp 
               tcp_hdr = (struct tcphdr *) ip_hdr + sizeof(struct iphdr);
               // memcpy(&lb_it->hi.type, &pkt_data[50 + shift*8], 50);
