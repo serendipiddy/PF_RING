880d4047e827942c2e0a640fa598e21af29a6075
taki <iddyiddyiddy+kitkat@gmail.com>
2017-01-31 22:55:12 +1300

    trying to read using openflow.h

--- userland/examples_zc/zcount.c
+++ userland/examples_zc/zcount.c
@@ -240,11 +240,11 @@ void process_ofp(struct ofp_header * ofp) {
     
     if (ofp->type == OFPT_PACKET_IN) {
         struct ofp_packet_in* p = (struct ofp_packet_in*) ofp;
-        printf("PKT_IN: xid(%u) len(%hu)\n", ofp->xid), p->total_len;
+        printf("PKT_IN: xid(%u) len(%hu)\n", ofp->xid, p->total_len);
         printf("addresses: pktin(0x%X) ofp(0x%X)\n", p, ofp);
         printf("addresses: pktin(0x%X) ofp(0x%X)\n", p->header.type, ofp->type);
         printf("addresses: pktin(0x%X) ofp(0x%X)\n", &p->header.type, &ofp->type);
-        printf("addresses: pktin(%h)\n", p->total_len);
+        printf("addresses: pktin(%hu)\n", p->total_len);
         
         printf("PKT_IN:\n  %02X%02X%02X%02X%02X%02X\n  %02X%02X%02X%02X%02X%02X\n", 
             *( ((char *) p) ), *( ((char *) p) +1), *( ((char *) p) +2), *( ((char *) p) +3), *( ((char *) p) +4), *( ((char *) p) +5), 
