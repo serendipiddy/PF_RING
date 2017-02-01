f5b396369f58e503362a2231efa0f13e7191e993
taki <iddyiddyiddy+kitkat@gmail.com>
2017-02-01 21:52:58 +1300

    added column for ofp type (with 2B padding)

--- userland/examples_zc/lock_buffer.c
+++ userland/examples_zc/lock_buffer.c
@@ -14,8 +14,8 @@ struct id_time {
     u_char dst[6];
     u_char src[6];
     u_char ofp_mac[6];
-    u_char ofp_type;
-    u_char padding[3];
+    u_int8_t ofp_type;
+    u_char padding[2];
 };
 
 /* Lock buffer file pointer */
