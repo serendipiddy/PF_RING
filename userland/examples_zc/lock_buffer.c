c85976dc19e86712dfc020c53be70b90d9be3149
taki <iddyiddyiddy+kitkat@gmail.com>
2017-02-02 01:04:56 +1300

    changed hwts to be 64 bit int

--- userland/examples_zc/lock_buffer.c
+++ userland/examples_zc/lock_buffer.c
@@ -10,8 +10,8 @@ struct id_time {
     u_int32_t id;
     u_int32_t sec;
     u_int32_t nsec;
-    // u_char hwts[8];
-    u_int64_t hwts;
+    u_char hwts[8];
+    // u_int64_t hwts;
     u_char dst[6];
     u_char src[6];
     u_char ofp_mac[6];
